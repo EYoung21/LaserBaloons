00c374b488e63568c61d87fc83ca1a95a8b28af5
Eli Young <eliyoung4now@gmail.com>
2025-01-28 13:14:16 +0100

    Initial commit

---
+++ Assets/Scripts/Poof.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script handles the balloon popping animation on the destroyed Balloon. 
+/// It requires a SpriteRenderer, as the SpriteRenderer is used in the script to cycle through the sprites of the animation.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class Poof : MonoBehaviour
+{
+
+    [Tooltip("The individual sprites of the animation")]
+    public Sprite[] frames;
+    [Tooltip("How fast does the animation play")]
+    public float framesPerSecond;
+    [Tooltip("An Audioclip with the sound that is played when the poof happens")]
+    public AudioClip poofSound;
+    SpriteRenderer spriteRenderer;
+
+    // Wait this amount of seconds to destroy the game object.
+    float destroyTimer = 1;
+
+    /// <summary>
+    /// Start is called by Unity. This will play our poof sound
+    /// </summary>
+    void Start()
+    {
+		if (poofSound != null) {
+			AudioSource.PlayClipAtPoint(poofSound, transform.position);
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // In Update you should add the sprite animation as shown on the lecture slides.
+    private void Update()
+    {
+        destroyTimer -= Time.deltaTime;
+        if(destroyTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+/// <summary>
+/// This class handles the soldier that shoots the balloons.
+/// A laser is cast from the LaserStart object to where the mouse position on the screen is when the left mouse button is pressed.
+/// Use a RayCast to check if the laser hits any balloons.
+/// </summary>
+public class Soldier : MonoBehaviour {
+
+	[Tooltip("GameObject at the position where the laser starts")]
+	public GameObject laserStart;
+	[Tooltip("Crosshair GameObject")]
+	public GameObject crosshair;
+	[Tooltip("LineRenderer of the laser graphics")]
+	public LineRenderer laserLineRenderer;
+	[Tooltip("The layer of the balloons, for easy raycasting only agains balloons")]
+	public LayerMask balloonLayerMask;
+
+	void Update () {
+		
+		// use GetMouseWorldPosition() and UpdateCrosshair() to make the chrosshair move with the mouse
+		Vector3 mouseWorldPos = GetMouseWorldPosition();
+
+		UpdateCrosshair(mouseWorldPos);
+
+		var origin = laserStart.transform.position;
+		var target = crosshair.transform.position;
+
+		var direction = target - origin;
+
+		var distancex = target.x - origin.x;
+		var distancey = target.y - origin.y;
+
+		var distanceOverall = Math.Sqrt((distancex * distancex) + (distancey*distancey));
+
+	}
+
+    /// <summary>
+    /// Grabs the world position of the mouse with z = 0.
+    /// </summary>
+    /// <returns>World position of mouse as Vector3</returns>
+    Vector3 GetMouseWorldPosition() {
+        // this gets the current mouse position (in screen coordinates) and transforms it into world coordinates
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // the camera is on z = -10, so all screen coordinates are on z = -10. To be on the same plane as the game, we need to set z to 0
+        mouseWorldPos.z = 0;
+
+        return mouseWorldPos;
+    }
+
+	/// <summary>
+	/// Updates the crosshair position and the line renderer from the laser to point from laserStart to the crosshair
+	/// </summary>
+	void UpdateCrosshair(Vector3 newCrosshairPosition){
+		crosshair.transform.position = newCrosshairPosition;
+		laserLineRenderer.SetPosition (0, laserStart.transform.position);
+		laserLineRenderer.SetPosition (1, crosshair.transform.position);
+	}
+}
